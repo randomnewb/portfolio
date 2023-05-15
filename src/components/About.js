6f12242722e6c95fc5ee2000a12aef19d7f7f05f
randomnewb <nguy1581@gmail.com>
2023-05-14 21:00:37 -0500

    Color and styles update, added Github to navbar

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ export default function About() {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+              className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg"
             >
               Want to Chat? Contact Me.
             </a>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,6 +17,9 @@ export default function Navbar() {
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
+          <a href="#github" className="mr-5 hover:text-white">
+            Github
+          </a>
         </nav>
         <a
           href="#contact"
